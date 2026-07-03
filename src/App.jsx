d47063f82a3d31f31b034dda6f69372238558c0e
Urooj Sheikh <s.urooj197@gmail.com>
2026-07-03 14:31:10 +0500

    Day 3: React Router setup with Overview, Inventory, Settings, Reports pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,23 @@
-import Greeting from './Greeting';
-import Counter from './Counter';
-import ToggleText from './ToggleText';
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
+import Overview from './pages/Overview';
+import Inventory from './pages/Inventory';
+import Settings from './pages/Settings';
+import Reports from './pages/Reports';
 
 function App() {
   return (
-    <div>
-      <Greeting name="Urooj" role="Frontend" />
-      <Counter />
-      <ToggleText />
-    </div>
+    <BrowserRouter>
+      <nav>
+        <Link to="/">Overview</Link> | <Link to="/inventory">Inventory</Link> | <Link to="/settings">Settings</Link> | <Link to="/reports">Reports</Link>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Overview />} />
+        <Route path="/inventory" element={<Inventory />} />
+        <Route path="/settings" element={<Settings />} />
+        <Route path="/reports" element={<Reports />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/pages/Overview.jsx
@@ -0,0 +1,5 @@
+function Overview() {
+  return <h1>Overview Page</h1>;
+}
+
+export default Overview;
